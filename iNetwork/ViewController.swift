f718a7cdab19a478c55c1848b9f8f0bf292e311a
ANI <ani@gm009-9.local>
2017-01-31 20:50:50 -0500

    In class networking example done.

--- iNetwork/ViewController.swift
+++ iNetwork/ViewController.swift
@@ -15,6 +15,70 @@ class ViewController: UIViewController {
     
     @IBOutlet var networkStatusLabel :UILabel!
     
+    //MARK: - Core Methods
+    
+    func parseJson(data: Data) {
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String:Any]
+            print("JSON:\(jsonResult)")
+            let flavorsArray = jsonResult["flavors"] as! [[String:Any]]
+            for flavorDictionary in flavorsArray {
+                print("Flavor:\(flavorDictionary)")
+            }
+            for flavorDictionary in flavorsArray {
+                print("Flavor:\(flavorDictionary["name"]!)")
+            }
+        } catch {
+            print("JSON Parsing Error")
+        }
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+        
+    }
+    
+    func getFile(filename: String) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let urlString = "http://\(hostName)\(filename)"
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 30
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard let receivedData = data else {
+                print("No Data")
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                return
+            }
+            if receivedData.count > 0 && error == nil {
+                print("Received Data:\(receivedData)")
+                let dataString = String.init(data: receivedData, encoding: .utf8)
+                print("Got Data String:\(dataString!)")
+                self.parseJson(data: receivedData)
+            } else {
+                print("Got Data of Length 0")
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+        }
+        task.resume()
+    }
+    
+    //MARK: - Interactivity Methods
+    
+    @IBAction func getFilePressed(button: UIButton) {
+        guard let reach = reachability else {
+            return
+        }
+        if reach.isReachable {
+            //getFile(filename: "/classfiles/iOS_URL_Class_Get_File.txt")
+            getFile(filename: "/classfiles/flavors.json")
+        } else {
+            print("Host Not Reachable. Turn on the Internet")
+        }
+
+    }
+    
     //MARK: - Reachability Methods
     
     func setupReachability(hostName: String) {

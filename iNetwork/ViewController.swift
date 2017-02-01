9354b5a732fe5fa55626efdc766fff93830fd6df
ANI <ani@gm009-9.local>
2017-01-31 19:33:25 -0500

    Network status indicator is working.

--- iNetwork/ViewController.swift
+++ iNetwork/ViewController.swift
@@ -9,15 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let hostName = "www.moveablebytes.com"
+    var reachability :Reachability?
+    
+    @IBOutlet var networkStatusLabel :UILabel!
+    
+    //MARK: - Reachability Methods
+    
+    func setupReachability(hostName: String) {
+        reachability = Reachability(hostname: hostName)
+        reachability!.whenReachable = { reachability in
+            DispatchQueue.main.async {
+                self.updatedLabel(reachable: true, reachability: reachability)
+            }
+        }
+        reachability!.whenUnreachable = { reachability in
+            
+        }
+    }
+    
+    func startReachability() {
+        do {
+            try reachability!.startNotifier()
+        } catch {
+            networkStatusLabel.text = "Unable to Start Notifier"
+            networkStatusLabel.textColor = .red
+            return
+        }
+    }
+    
+    func updatedLabel(reachable: Bool, reachability: Reachability) {
+        if reachable {
+            if reachability.isReachableViaWiFi {
+                networkStatusLabel.textColor = .green
+            } else {
+                networkStatusLabel.textColor = .blue
+            }
+        } else {
+            networkStatusLabel.textColor = .red
+        }
+        networkStatusLabel.text = reachability.currentReachabilityString
+    }
+    
+    //MARK: - Life Cycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupReachability(hostName: hostName)
+        startReachability()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
